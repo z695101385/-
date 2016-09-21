3ebad10a464017b27bcc33b0584fd11e300dbcb5
z695101385 <695101385@qq.com>
2016-09-21 14:26:18 +0800

    初始化

---
+++ News163/News163/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  News163
+//
+//  Created by 张晨 on 2016/9/21.
+//  Copyright © 2016年 张晨. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
